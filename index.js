006a3c0d8e9f20d6d5c509315ff8d3f761e250dd
UupDownPark <geakura123@gmail.com>
2023-12-04 23:12:02 +0900

    query

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ let server = require("./server");
 let router = require("./router");
 let requestHandler = require("./requestHandler");
 
+const mariadb = require("./database/connect/mariadb");
+mariadb.connect();
+
 server.start(router.router, requestHandler.handle);
 //모듈을 사용하는 역할
--- requestHandler.js
+++ requestHandler.js
@@ -1,4 +1,9 @@
+const mariadb = require("./database/connect/mariadb");
+
 function main(response) {
+  mariadb.query("SELECT * FROM product", function (err, rows) {
+    console.log(rows);
+  });
   response.writeHead(200, { "Content-Type": "text/html" });
   response.write("Hello Node.js");
   response.end();
